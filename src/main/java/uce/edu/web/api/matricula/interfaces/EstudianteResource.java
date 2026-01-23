a4e0c12caa6485f0364a0c8e4a71e0905386b7de
AlexisETB <alexistroya22@gmail.com>
2026-01-22 19:43:46 -0500

    feat: Add JAXB dependency and update EstudianteResource to support JSON and XML responses

--- src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
@@ -10,7 +10,9 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.matricula.application.EstudianteService;
 import uce.edu.web.api.matricula.domain.Estudiante;
@@ -23,6 +25,7 @@ public class EstudianteResource {
 
     @GET
     @Path("")
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Estudiante> listarTodos() {
         System.out.println("Listar todos los estudiantes");
         return this.estudianteService.listarTodos();
@@ -30,6 +33,7 @@ public class EstudianteResource {
 
     @GET
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_XML)
     public Estudiante consultarPorId(@PathParam("id") Integer id) {
         return this.estudianteService.consultarPorId(id);
     }
@@ -69,6 +73,7 @@ public class EstudianteResource {
 
     @GET
     @Path("/provincia/genero")
+    @Produces(MediaType.APPLICATION_XML)
     public List<Estudiante> buscarPorProvinciaGenero(@QueryParam("provincia") String provincia,
             @QueryParam("genero") String genero) {
         return this.estudianteService.buscarPorProvinciaGenero(provincia, genero);
