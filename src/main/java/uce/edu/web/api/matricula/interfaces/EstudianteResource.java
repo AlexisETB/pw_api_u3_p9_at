075558cf904bfee2db36c101f4f3c9188c52c22d
AlexisETB <alexistroya22@gmail.com>
2026-01-22 19:16:42 -0500

    feat: Update guardar and actualizarParcial methods in EstudianteResource to return Response objects

--- src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/EstudianteResource.java
@@ -11,6 +11,7 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.matricula.application.EstudianteService;
 import uce.edu.web.api.matricula.domain.Estudiante;
 
@@ -35,8 +36,9 @@ public class EstudianteResource {
 
     @POST
     @Path("")
-    public void guardar(Estudiante estudiante) {
+    public Response guardar(Estudiante estudiante) {
         this.estudianteService.crearEstudiante(estudiante);
+        return Response.status(Response.Status.CREATED).entity(estudiante).build();
     }
 
     @PUT
@@ -47,8 +49,9 @@ public class EstudianteResource {
 
     @PATCH
     @Path("/{id}")
-    public void actualizarParcial(@PathParam("id") Integer id, Estudiante estudiante) {
+    public Response actualizarParcial(@PathParam("id") Integer id, Estudiante estudiante) {
         this.estudianteService.actualizarParcial(id, estudiante);
+        return Response.status(209).entity(null).build();
     }
 
     @DELETE
